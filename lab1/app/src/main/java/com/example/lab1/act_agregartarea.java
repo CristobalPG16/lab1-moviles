5e622b6ae621091e9367a2e48e58bdbc5172b486
Cristobal Pérez García <cristobal.pg16@gmail.com>
2021-10-01 22:18:22 -0600

    arreglos de interfaz y navegacion

--- lab1/app/src/main/java/com/example/lab1/act_agregartarea.java
+++ lab1/app/src/main/java/com/example/lab1/act_agregartarea.java
@@ -2,14 +2,35 @@ package com.example.lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class act_agregartarea extends AppCompatActivity {
+
+    Button btn_guardar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lyt_agregartarea);
+
+
+        btn_guardar = findViewById(R.id.btn_guardar);
+
+        btn_guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(act_agregartarea.this, act_consultartareas.class);
+                startActivity(intent);
+
+                //prueba de que el boton escucha, es un mensaje
+                //Toast.makeText(getApplicationContext(),"Bienvenido ", Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
     }
 }
--- lab1/app/src/main/java/com/example/lab1/act_consultartareas.java
+++ lab1/app/src/main/java/com/example/lab1/act_consultartareas.java
@@ -2,14 +2,36 @@ package com.example.lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class act_consultartareas extends AppCompatActivity {
+
+    Button btn_agregar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lyt_tareas);
+
+
+        btn_agregar = findViewById(R.id.btn_agregar);
+
+        btn_agregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(act_consultartareas.this, act_agregartarea.class);
+                startActivity(intent);
+
+                //prueba de que el boton escucha, es un mensaje
+                //Toast.makeText(getApplicationContext(),"Bienvenido ", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
 
     }
 }
