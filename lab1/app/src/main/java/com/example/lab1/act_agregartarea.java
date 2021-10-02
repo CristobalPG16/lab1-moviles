4ca4977fdd20afe235a447f8966d747b78a1630c
Cristobal Pérez García <cristobal.pg16@gmail.com>
2021-10-01 19:40:57 -0600

    creacion del proyecto

---
+++ lab1/app/src/main/java/com/example/lab1/act_agregartarea.java
@@ -0,0 +1,15 @@
+package com.example.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class act_agregartarea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lyt_agregartarea);
+
+    }
+}
